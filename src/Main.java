5acde37e3e4f88ab574cd375b3862318b477efb1
AlexSvobo <159861229+AlexSvobo@users.noreply.github.com>
2024-03-25 22:42:07 -0400

    File Merge
    Correcting files and directory from original upload.

--- src/Main.java
+++ src/Main.java
@@ -1,41 +1,48 @@
-// Alex Svoboda
-// a400@umbc.edu
+//Alex Svoboda
+
 import java.util.Scanner;
-import java.util.Random;
 
 public class Main {
-
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
+            Scanner input = new Scanner(System.in);
 
-        // Ask a user to enter first name and last name. (Allow for mixed case)
-        System.out.print("Enter your first name. ");
-        String firstName = input.nextLine().replaceAll("\\s+", "").toUpperCase();
-
-        System.out.print("Enter your last name and press enter: ");
-        String lastName = input.nextLine().replaceAll("\\s+", "").toLowerCase().substring(0,3);
-
-        //Returns the last letter of first name, capitalized
-        char firstNameLetter = firstName.charAt(firstName.length() - 1);
-
-        //Create random integer from 10-99
-        int randNum = new Random().nextInt(90) + 10;
-        //Create password string
-        String password = String.valueOf(firstNameLetter)+randNum+lastName;
-
-        System.out.println(("Your password is: " + password));
-
-        //Use concat method
-        String newPassword = password.concat("****");
-
-        System.out.println(("Your password using concat: " + newPassword)); // debugging
-
-        //Reverse the result of the string and print
-        System.out.printf("Your new password after reverse: %s", new StringBuilder(newPassword).reverse());
+            final double FEE_RATE = 0.25;
+            byte hours, payRate;
+            boolean delinquent;
+            String name, address, creditor, hardship; 
+            char fullTime;
 
 
+            System.out.println("Welcome to the IS147 Project System!\nPlease follow the instructions provided on the screen\n----------------------------");
 
+            // Prompt the user to enter their information
+            System.out.print("Enter the total debt amount: $");
+            int totalDebt = input.nextLine();
 
-        }
+            System.out.print("What is your age?: ");
+            byte age = input.nextByte();
+
+            System.out.print("Have you experienced any financial hardship?: Y/N");
+            boolean hardship = input.next();
+
+            System.out.print("What is your hourly pay rate?: ");
+            payRate = input.nextByte();
+
+            System.out.println("*****************************************");
+            System.out.println("Hello, You are: " + age + " years old");
+            System.out.println("Your total debt amount is: " +  totalDebt);
+            System.out.println("Vacation request: true");
+
+            double grossPay = payRate*hours;
+            System.out.println("Gross Pay: $" + grossPay);
+
+            double taxPaid = TAX_RATE*grossPay;
+            System.out.println("Tax paid: $" + taxPaid);
+            System.out.println("--------------------");
+
+            double netPay = grossPay-taxPaid;
+            System.out.println("Net pay: $" +netPay );
+            System.out.println("*************************************************");
 
     }
+}
